a8c140866fc9085701df9c18e87758dd1aa28303
Billgo <cocobill@vip.qq.com>
2020-10-08 18:09:11 +0800

    publish to v0.3.1

--- react.js
+++ react.js
@@ -1,21 +1,22 @@
 "use strict";
 
 module.exports = {
-  extends: [
-    "./index",
-    "./lib/rules/browser",
-    "./lib/rules/import",
-    "./lib/rules/react",
-    "./lib/rules/react-a11y",
-  ].map(require.resolve),
+  extends: ["./index", "./lib/rules/browser", "./lib/rules/import", "./lib/rules/react", "./lib/rules/react-a11y"].map(
+    require.resolve,
+  ),
   parser: "babel-eslint",
   rules: {},
   parserOptions: {
+    ecmaVersion: 2020,
     ecmaFeatures: {
       jsx: true,
       experimentalObjectRestSpread: true,
       impliedStrict: true,
       classes: true,
     },
+  },
+  env: {
+    browser: true,
+    jest: true,
   },
 };
