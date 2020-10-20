641ac407a7032cdf05e5fbafa9bf827cd48f66c4
Billgo <cocobill@vip.qq.com>
2020-10-20 18:19:06 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const builtin = ["./lib/legacy", "./lib/rules/es6", "./lib/rules/es2020"].map(
-  require.resolve,
-);
+const builtin = ["./lib/legacy", "./lib/rules/es6", "./lib/rules/es2020"].map(require.resolve);
 
 module.exports = {
   extends: builtin.concat(["plugin:eggache/recommended"]),
